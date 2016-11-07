c35e1c07b1d32f4671038eaa6c362679b2459880
Anan McHarris <am7155@nyu.edu>
2016-11-07 00:39:28 -0500

    First Commit: have basic movement, mouse sight working.

---
+++ Assets/scripts/mouseScript.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class mouseScript : MonoBehaviour {
+	public Transform catTransform;
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		Vector3 directionToCat = catTransform.position - transform.position;
+		//Debug.Log (directionToCat);
+
+		Debug.DrawRay (transform.position, directionToCat, Color.green);
+
+		if (Vector3.Angle(transform.forward, directionToCat) < 180) {
+			//Debug.Log ("Woa dere");
+			Ray mouseRay = new Ray (transform.position, directionToCat);
+			RaycastHit mouseHitRayInfo = new RaycastHit ();
+		
+			if (Physics.Raycast(mouseRay, out mouseHitRayInfo, 5f)){
+				if (mouseHitRayInfo.collider.tag == "cat") {
+					Debug.Log ("Run!");
+				}
+			}
+		}
+	}
+}
---
+++ Assets/scripts/movementScript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class movementScript : MonoBehaviour {
+
+	// Update is called once per frame
+	void FixedUpdate () {
+		GetComponent<Rigidbody>().velocity = transform.forward * 10f + Physics.gravity;
+
+		Ray newRay = new Ray (transform.position, transform.forward);
+
+		if (Physics.SphereCast(newRay,.25f,1f) == true){
+			float randomFloat = Random.value;
+			if (randomFloat < .5f) {
+				transform.Rotate (new Vector3 (0, 90, 0));
+			} else {
+				transform.Rotate (new Vector3 (0, -90, 0));
+			}
+		}
+	}
+}
